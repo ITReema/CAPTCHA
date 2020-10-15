3234d06a51991b6efcfb1c1160c2a3552fff98d2
Reema <it.reema7@gmail.com>
2020-10-15 04:24:48 +0300

    Update alert_captcha.php

--- Text_Based_CAPTCHA/assets/alert_captcha.php
+++ Text_Based_CAPTCHA/assets/alert_captcha.php
@@ -1,25 +1,30 @@
 <?php
+// Starting session
 session_start();
+// collect value of input field
 if(!empty($_POST["submit"])) {
   $captcha = $_POST["captcha"];
-
   //alert captcha status
   $captcha_text = $_POST["captcha"];
+  // if user doesn't enter CAPTCHA
   if(empty($captcha)) {
     $captchaError = array(
       "status" => "alert-danger",
-      "alert" => "Please enter the captcha."
+      "alert" => "Please enter the characters in the image"
     );
   }
+  // if user enter correct CAPTCHA
   else if($_SESSION['captcha'] == $captcha_text){
     $captchaError = array(
       "status" => "alert-success",
-      "alert" => "Correct Captcha."
+      "alert" => "Correct CAPTCHA"
     );
-  } else {
+  } 
+  // if user enter invalid CAPTCHA
+  else {
     $captchaError = array(
-      "status" => "alert-danger",
-      "alert" => "Invalid Captcha."
+      "status" => "alert-warning",
+      "alert" => "Invalid CAPTCHA, please try again"
     );
   }
 }  
